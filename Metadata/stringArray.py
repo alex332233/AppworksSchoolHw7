f8bace2cab3b6058d2972f2bd0500a782f06cc39
Alex Su <alex332233@gmail.com>
2023-10-22 23:23:20 +0800

    add Homework7

---
+++ Metadata/stringArray.py
@@ -0,0 +1,9 @@
+[
+    "https://ipfs.io/ipfs/QmT3bPfYodTRdKgPDvZdWf15RvQvGgpzpCA97sXAUfTTZp",
+    "https://ipfs.io/ipfs/QmRKqekmWyYB3sFbgJ8op1rS3wEoewkbkodhCVuFvBQVnd",
+    "https://ipfs.io/ipfs/QmZ4vK3FHs7sFkgD3iBJPS88vdWFvigC595VxjKkwJBcgN",
+],
+[
+    "https://ipfs.io/ipfs/QmPvUB9AuMCw9gfyxVMXaqUQhBP4yUDMG1KCqDfdYTNv8Z",  # 盲盒img
+    "https://ipfs.io/ipfs/QmYwfhxyg8xo4MQ7PLFsBsK1nDiB4BHJyFvbn3trb6iw6q",  # 盲盒metadata
+]
